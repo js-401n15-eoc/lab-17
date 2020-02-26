48c6d4b50504ae1021cce7ff069eb884117dac29
Earl Jay Caoile <ecaoile@my.hpu.edu>
2020-02-25 18:01:39 -0800

    added UML diagram

--- app/app.js
+++ app/app.js
@@ -5,7 +5,6 @@ const client = new net.Socket();
 
 client.connect(3001, 'localhost', () => console.log('app connected? yes'));
 require('./logger.js');
-const file = `${__dirname}/files/chicken-scratch.txt`;
 const alterFile = require('./alter-file.js');
 
 client.on('data', payload => {
